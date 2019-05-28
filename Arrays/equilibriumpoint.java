44578ba124754a7f1126f0b5c865f39096527aba
prajat <rajat.singh1084@gmail.com>
2019-05-28 23:11:26 +0530

    ques added

---
+++ Arrays/equilibriumpoint.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.*;
+/**
+ * equilibriumpoint
+ */
+public class equilibriumpoint {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while (t-- > 0) {
+            int n = s.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = s.nextInt();
+                
+
+            }
+        }
+    }
+}
