aa0cc513ef0c832c765e12c79dacb55dcc5609fc
prajat <rajat.singh1084@gmail.com>
2019-05-28 14:09:56 +0530

    ques solved

--- Arrays/missinnoinarray.java
+++ Arrays/missinnoinarray.java
@@ -4,6 +4,23 @@ import java.util.*;
 public class missinnoinarray {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while (t-- > 0) {
+            int n = s.nextInt();
+            int[] arr = new int[n - 1];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = s.nextInt();
+
+            }
+            int totalsum = (n * (n + 1)) / 2;
+            int currsum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                currsum += arr[i];
+            }
+            System.out.println(totalsum-currsum);
+
+        }
         
     }
 }
