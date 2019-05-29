66faaee26028e11e47e2c47076fabe68986ee7e0
prajat <rajat.singh1084@gmail.com>
2019-05-29 14:08:15 +0530

    ques solved

--- Arrays/equilibriumpoint.java
+++ Arrays/equilibriumpoint.java
@@ -14,9 +14,28 @@ public class equilibriumpoint {
             int[] arr = new int[n];
             for (int i = 0; i < arr.length; i++) {
                 arr[i] = s.nextInt();
-                
-
             }
+
+            int flag = 0;
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                sum += arr[i];
+            }
+            int leftsum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                sum = sum - arr[i];
+                if (leftsum == sum) {
+                    System.out.println(i + 1);
+                    flag = 1;
+                    break;
+                }
+                leftsum += arr[i];
+            }
+            if (flag == 0) {
+                System.out.println(-1);
+            }
+
+            
         }
     }
 }
