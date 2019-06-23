15143b74b70c6a5ec096a8b3affe916a15e5bb7b
prajat <rajat.singh1084@gmail.com>
2019-06-23 15:47:01 +0530

    ques solved

--- Queues/maxofallsubarrayofsizek.java
+++ Queues/maxofallsubarrayofsizek.java
@@ -1,6 +1,6 @@
-import java.util.*;
-
 package Queues;
+
+import java.util.*;
 
 /**
  * maxofallsubarrayofsizek
@@ -18,19 +18,46 @@ public class maxofallsubarrayofsizek {
                 arr[i] = s.nextInt();
 
             }
-            int max = Integer.MIN_VALUE;
-            for (int i = 0; i < n - k + 1; i++) {
-                for (int j = i; j < i + k; j++) {
-                    if (arr[i] > max) {
-                        max = arr[i];
-                    }
+            // int max = Integer.MIN_VALUE;
+            // for (int i = 0; i < n - k + 1; i++) {
+            // for (int j = i; j < i + k; j++) {
+            // if (arr[i] > max) {
+            // max = arr[i];
+            // } o(n2) approach
+
+            // }
+            // System.out.print(max + " ");
+            // max = Integer.MIN_VALUE;
+            // }
+            // System.out.println();
+
+            int i;
+            Deque<Integer> queue = new LinkedList<>();
+            for (i = 0; i < k; ++i) {
+                while (!queue.isEmpty() && arr[i] >= arr[queue.peekLast()])
+                    ;
+                {
+                    queue.removeLast();
+                }
+                queue.addLast(i);
+            }
+            for (; i < n; i++) {
+                System.out.print(arr[queue.peek()] + " ");
+
+                while (!queue.isEmpty() && queue.peek() <= i - k) {
+                    queue.removeFirst();
 
                 }
-                System.out.print(max + " ");
-                max = Integer.MIN_VALUE;
+                while (!queue.isEmpty() && arr[i] >= arr[queue.peekLast()])
+                    ;
+                {
+                    queue.removeLast();
+                }
+
+                queue.addLast(i);
             }
-            System.out.println();
+            System.out.print(arr[queue.peek()]);
         }
-        
+
     }
 }
